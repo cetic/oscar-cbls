1a405085fe7625ab5ffb0cc3d0be30bdf0f4470b
Fabian Germeau <fabian.germeau@cetic.be>
2020-01-14 14:25:39 +0100

    ObjFunction display : percentile from Option[Double] to Int with 100 == 100%...

--- oscar-cbls/src/main/examples/oscar/examples/cbls/routing/SimpleVRPWithTimeWindowsAndVehicleContent.scala
+++ oscar-cbls/src/main/examples/oscar/examples/cbls/routing/SimpleVRPWithTimeWindowsAndVehicleContent.scala
@@ -206,7 +206,7 @@ object SimpleVRPWithTimeWindowsAndVehicleContent extends App{
 
 
   val search = bestSlopeFirst(List(oneChainInsert,oneChainMove,onePtMove(20)))afterMove({
-    enoughSpaceAfterNeighborNow = CapacityHelper.enoughSpaceAfterNeighbor(n,capacityInvariant)}) showObjectiveFunction(obj, percentile = Some(30))
+    enoughSpaceAfterNeighborNow = CapacityHelper.enoughSpaceAfterNeighbor(n,capacityInvariant)}) showObjectiveFunction(obj, percentile = 30)
   //val search = (BestSlopeFirst(List(routeUnroutdPoint2, routeUnroutdPoint, vlsn1pt)))
 
 
