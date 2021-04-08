b30e500a5071aab0c752ba8b86a01a0991164552
rdl <rdl@cetic.be>
2021-04-08 13:24:18 +0200

    removed "coucou"

--- oscar-cbls/src/main/scala/oscar/cbls/algo/seq/IntSequence.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/algo/seq/IntSequence.scala
@@ -773,7 +773,6 @@ abstract class StackedUpdateIntSequence(depth:Int) extends IntSequence(depth = d
     require(pos >= 0, "pos=" + pos + " for delete on UniqueIntSequence should be >= 0")
     require(pos < size, "cannot delete past end of sequence in UniqueIntSequence")
     if(depth >= 20){
-      println("coucou3")
       new RemovedIntSequence(this,pos, depth+1).commitPendingMoves
     }else{
       new RemovedIntSequence(this,pos, depth+1)
@@ -785,7 +784,6 @@ abstract class StackedUpdateIntSequence(depth:Int) extends IntSequence(depth = d
     require(endPositionIncluded >= 0 && endPositionIncluded < size , "endPositionIncluded=" + endPositionIncluded +" should be in [0,size"+size+"[ in UniqueIntSequence.moveAfter")
     require(moveAfterPosition >= -1 && moveAfterPosition < size , "moveAfterPosition=" + moveAfterPosition + " should be in [-1,size="+size+"[ in UniqueIntSequence.moveAfter")
     if(depth >= 20) {
-      println("coucou2")
       new MovedIntSequence(this,startPositionIncluded,endPositionIncluded,moveAfterPosition,flip,depth+1).commitPendingMoves
     }else{
       new MovedIntSequence(this,startPositionIncluded,endPositionIncluded,moveAfterPosition,flip,depth+1)
@@ -795,8 +793,6 @@ abstract class StackedUpdateIntSequence(depth:Int) extends IntSequence(depth = d
   override def insertAtPosition(value : Int, pos : Int, fast:Boolean,autoRework:Boolean) : IntSequence = {
     require(pos >= 0 && pos <= size , "pos=" + pos + " should be in [0,size="+size+"] in IntSequence.insertAt")
     if(depth >= 20){
-
-      println("coucou")
       new InsertedIntSequence(this,value:Int,pos:Int, depth+1).commitPendingMoves
     }else{
       new InsertedIntSequence(this,value:Int,pos:Int, depth+1)
