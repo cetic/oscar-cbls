074a099385b515f2dd7e4c25e04a05d11d47dc92
Thomas Fayolle <thomas.fayolle@cetic.be>
2021-09-16 13:35:45 +0200

    Adding new version of naive constraint

---
+++ oscar-cbls/src/main/scala/oscar/cbls/business/routing/invariants/naive/ForwardNaiveConstraint.scala
@@ -0,0 +1,47 @@
+package oscar.cbls.business.routing.invariants.naive
+
+import oscar.cbls.core.computation.ChangingSeqValue
+import oscar.cbls.algo.seq.IntSequenceExplorer
+import oscar.cbls.business.routing.invariants.segments.{Segment,NewNode,PreComputedSubSequence,FlippedPreComputedSubSequence}
+import oscar.cbls.business.routing.invariants.segments.VehicleSegments
+import oscar.cbls.algo.quick.QList
+
+
+
+abstract class ForwardNaiveRoutingConstraint[U <: Any : Manifest](routes : ChangingSeqValue,n : Int,v : Int,defaultValue : U ,initValuePerVehicle : Array[U],fonc : (Int,Int,U) => U) extends AbstractNaiveRoutingConstraint[U](routes,n,v,defaultValue,initValuePerVehicle,fonc) {
+
+  override def getNextPointOfSegmentToUpdate(expl : IntSequenceExplorer,seg : Segment) : Option[IntSequenceExplorer] =  {
+    seg match {
+      case NewNode(_) => None
+      case PreComputedSubSequence(_,lastNode,_) =>
+        if (expl.value == lastNode) None else expl.next
+      case FlippedPreComputedSubSequence(_,lastNode,_) =>
+        if (expl.value == lastNode) None else expl.next
+    }
+  }
+
+  override def firstPointToUpdateOnSegment(seg : Segment) : Int = {
+    seg match {
+      case NewNode(n) => n
+      case PreComputedSubSequence(fstNode,_,_) => fstNode
+      case FlippedPreComputedSubSequence(fstNode,_,_) => fstNode
+    }
+  }
+
+  override def lastPointToUpdateOnSegment(seg : Segment) : Int = {
+    seg match {
+      case NewNode(n) => n
+      case PreComputedSubSequence(_,lstNode,_) => lstNode
+      case FlippedPreComputedSubSequence(_,lstNode,_) => lstNode
+    }
+
+  }
+
+  override def ordonateSegments(segments : VehicleSegments) : QList[Segment] = {
+    segments.segments
+  }
+
+
+
+
+}
