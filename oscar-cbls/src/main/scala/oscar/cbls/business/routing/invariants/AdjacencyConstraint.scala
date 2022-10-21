8bda73f44cb3e5d7df5e6b5d22a1f5f8059a22b2
rdl <rdl@cetic.be>
2022-10-21 16:07:16 +0200

    polish

--- oscar-cbls/src/main/scala/oscar/cbls/business/routing/invariants/AdjacencyConstraint.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/business/routing/invariants/AdjacencyConstraint.scala
@@ -6,11 +6,11 @@ import oscar.cbls.core.computation.CBLSIntVar
 object AdjacencyConstraint {
   /**
     * 
-    * Build a adjacency contraints. The adjacency constraints is respected if the node of an adjacency pair are adjacent
+    * Build a adjacency constraints. The adjacency constraints is respected if the node of an adjacency pair are adjacent
     * The constraint returns the number of time the adjacency constraint is violated
     * 
     * @param vrp The vehicle routing problem
-    * @param perVehicle a boolean flag that tells if the value shall be coputed per vehicle
+    * @param perVehicle a boolean flag that tells if the value shall be computed per vehicle
     * @param respectOrder a boolean flag that tells is the adjacency shall respect the order
     * @param adjacencyList The list of pair of node that shall be adjacent
     * @return An array of CBLSIntVar that maintains the violation value. The array contains v value if the flag perVehicle is true and one value otherwise
@@ -23,20 +23,6 @@ object AdjacencyConstraint {
     adjacencyList : List[(Int,Int)]) : Array[CBLSIntVar] = {
 
     val distanceMatrix4Adjacency : Array[Array[Long]] = Array.fill(vrp.n)(Array.fill(vrp.n)(0))
-
-
-      // Array.tabulate(vrp.n)(i => {
-      //   Array.tabulate(vrp.n)(j => {
-      //     if (adjacencyList.contains((i,j)) || (!respectOrder && adjacencyList.contains((j,i)))) {
-      //       -2
-      //     } else {
-      //       if (i == j && (adjacencyList.map(_._1).contains(i) || adjacencyList.map(_._2).contains(i) || adjacencyList.map(_._1).contains(j) || adjacencyList.map(_._2).contains(j)))
-      //         1
-      //       else
-      //         0
-      //     }
-      //   })
-      // })
 
     adjacencyList.foreach({case (i,j) => {
       distanceMatrix4Adjacency(i)(i) = 1
@@ -53,5 +39,4 @@ object AdjacencyConstraint {
       !respectOrder)
 
   }
-
 }
