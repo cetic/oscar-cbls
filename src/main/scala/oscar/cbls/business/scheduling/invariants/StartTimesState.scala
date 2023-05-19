76608826d563a1b5706c8c1bcbfc9bd8fcc8b3b9
Thomas Fayolle <thomas.fayolle@cetic.be>
2023-05-19 10:36:31 +0200

    Removing all the files

--- src/main/scala/oscar/cbls/business/scheduling/invariants/StartTimesState.scala
+++
@@ -1,9 +0,0 @@
-package oscar.cbls.business.scheduling.invariants
-
-import oscar.cbls.business.scheduling.ActivityId
-import oscar.cbls.business.scheduling.model.{Resource, ResourceState}
-
-case class StartTimesState(resourceStates: Map[Resource, ResourceState],
-                           makeSpanValue: Int,
-                           activityId: ActivityId,
-                           activityStartTime: Int)
