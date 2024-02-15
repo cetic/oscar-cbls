70f9fcf8ff2a12c6980b830e18b38dcbed1b9b14
smi <stefano.michelini@cetic.be>
2024-02-15 10:33:37 +0100

    Move ShiftedIterable for readability

--- src/main/scala/oscar/cbls/algo/search/HotRestart.scala
+++ src/main/scala/oscar/cbls/algo/search/HotRestart.scala
@@ -44,47 +44,6 @@ object HotRestart {
     new ShiftedIterable(it, pivot, true)
 }
 
-class ShiftedIterable(it: Iterable[Int], pivot: Int, sequence: Boolean = false)
-    extends Iterable[Int] {
-  override def iterator: Iterator[Int] = {
-    if (sequence) {
-      // splitting into two parts:
-      val aboveIterator = it.iterator
-      def fetchHead: List[Int] = {
-        if (aboveIterator.hasNext) {
-          val nextValue = aboveIterator.next()
-          if (nextValue == pivot) List(nextValue)
-          else nextValue :: fetchHead
-        } else Nil
-      }
-      val below: List[Int] = fetchHead
-      new ShiftedIterator(aboveIterator.toList, below)
-    } else {
-      // TODO: maybe a lazy approach would be faster here?
-      val (above, below) = it.partition(i => i > pivot)
-      new ShiftedIterator(above, below)
-    }
-  }
-
-  class ShiftedIterator[@specialized(Int, Long) A](first: Iterable[A], var second: Iterable[A])
-      extends Iterator[A] {
-    // TODO: this is awful: maybe the stuff is already sorted
-    // TODO: we should perform a lazy sort since all the first might not be covered anyway
-    var it: Iterator[A] = first.toList.iterator
-    override def hasNext: Boolean = {
-      if (it.hasNext) true
-      else if (second == null) false
-      else {
-        it = second.toList.iterator
-        second = null
-        it.hasNext
-      }
-    }
-
-    override def next(): A = it.next()
-  }
-}
-
 /** this is an inclusive range.
   * @param start
   * @param end
@@ -165,5 +124,61 @@ class ShiftedSet(s: SortedSet[Int], pivot: Int) extends Iterable[Int] {
       currentValueReady = false
       currentValue
     }
+  }
+}
+
+class ShiftedIterable(it: Iterable[Int], pivot: Int, sequence: Boolean = false)
+  extends Iterable[Int] {
+  override def iterator: Iterator[Int] = {
+    if (sequence) {
+      // splitting into two parts:
+      val aboveIterator = it.iterator
+      def fetchHead: List[Int] = {
+        if (aboveIterator.hasNext) {
+          val nextValue = aboveIterator.next()
+          if (nextValue == pivot) List(nextValue)
+          else nextValue :: fetchHead
+        } else Nil
+      }
+      val below: List[Int] = fetchHead
+      new ShiftedIterator(aboveIterator.toList, below)
+    } else {
+      // TODO: maybe a lazy approach would be faster here?
+      val (above, below) = it.partition(i => i > pivot) // traverses collection twice
+      // also inconsistent with ShiftedRange, where the pivot (startBy) is the first element, not the last
+      // this is also the case for sorted sets
+      val l                  = List(1, 2, 3)
+      val (pqAbove, pqBelow) = (PQ[Int](), PQ[Int]())
+
+      it.foreach(i => if (i >= pivot) pqAbove.enqueue(i) else pqBelow.enqueue(i))
+
+      def foo(pqa: PQ[Int], pqb: PQ[Int]): Iterator[Int] = {
+        Iterator.tabulate(it.size)(_ => if (pqa.nonEmpty) pqa.dequeue() else pqb.dequeue())
+      }
+
+      new ShiftedIterator(above, below)
+    }
+  }
+
+  class ShiftedIterator[@specialized(Int, Long) A](first: Iterable[A], var second: Iterable[A])
+    extends Iterator[A] {
+    // TODO: this is awful: maybe the stuff is already sorted
+    // TODO: we should perform a lazy sort since all the first might not be covered anyway
+    // so there is no sorting happening here at all, meaning the elements are not given in increasing order
+    // idea: stuff them in priority queues?
+    // use view + to?
+    // also, why is this the only class not explicitly for Ints?
+    var it: Iterator[A] = first.toList.iterator
+    override def hasNext: Boolean = {
+      if (it.hasNext) true
+      else if (second == null) false
+      else {
+        it = second.toList.iterator
+        second = null
+        it.hasNext
+      }
+    }
+
+    override def next(): A = it.next()
   }
 }
