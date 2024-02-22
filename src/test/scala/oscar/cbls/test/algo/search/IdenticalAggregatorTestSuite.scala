18324a7084b128120b993704e231c1be7600bb3d
smi <stefano.michelini@cetic.be>
2024-02-22 10:27:34 +0100

    Add remaining test suite files

---
+++ src/test/scala/oscar/cbls/test/algo/search/IdenticalAggregatorTestSuite.scala
@@ -0,0 +1,9 @@
+package oscar.cbls.test.algo.search
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class IdenticalAggregatorTestSuite extends AnyFunSuite with ScalaCheckDrivenPropertyChecks with Matchers {
+
+}
