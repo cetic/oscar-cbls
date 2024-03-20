ece446cf9b86cb19d97f94ed54caa1f9df32af86
TFA <thomas.fayolle@cetic.be>
2024-03-20 13:44:48 +0100

    Adding comments in the propagation structure

--- src/test/scala/oscar/cbls/test/core/propagation/TestStructure/TestPropagationStructure.scala
+++ src/test/scala/oscar/cbls/test/core/propagation/TestStructure/TestPropagationStructure.scala
@@ -1,15 +1,37 @@
 package oscar.cbls.core.propagation
 
-class TestPropagationStructure(val debugLevel : Int = 0) extends PropagationStructure(debugLevel) {
+/** A propagation structure aimed at testing the propagation functions.
+  *
+  * The idea of this structure is the following:
+  *   - There are two types of elements: invariants and variables
+  *   - The invariant have variables as input
+  *   - Variables can be defined as invariants (they are the output of the invariant)
+  *
+  * In this structure, the invariants and variables do not compute anything; the variables can be
+  * updated (whatever an update means...); when a variable is updated, it is registered for
+  * propagation; propagating a variable means notifying the listening invariants of this variable;
+  * when a invariant is notified by an input variable, it updates its ouput variable
+  *
+  * Moreover, the structure maintains flags to know which elements need to be updated during a
+  * propagation and checks if the number of time each element is updated.
+  *
+  * @param debugLevel
+  *   the debug level (same debug level as the propagation structure)
+  */
 
+class TestPropagationStructure(val debugLevel: Int = 0) extends PropagationStructure(debugLevel) {
+
+  // The elements of the propagation structure
   def elements: List[TestPropagationElement] =
     getPropagationElements.map(_.asInstanceOf[TestPropagationElement])
 
+  // The invariants of the propagation structure
   def invariants: List[TestInvariantElement] =
     getPropagationElements
       .filter(_.isInstanceOf[TestInvariantElement])
       .map(_.asInstanceOf[TestInvariantElement])
 
+  // The variables of the propagation structure
   def variables: List[TestVariableElement] =
     getPropagationElements.flatMap(e =>
       if (e.isInstanceOf[TestVariableElement]) Some(e.asInstanceOf[TestVariableElement]) else None
@@ -21,40 +43,36 @@ class TestPropagationStructure(val debugLevel : Int = 0) extends PropagationStru
 
   private var partialPropagationTarget: List[TestPropagationElement] = List()
 
-  def myToDot(target : Option[TestPropagationElement] = None) : String = {
-    val labels = elements.map(e => {
-      val shapeString = if (e.isInstanceOf[TestInvariantElement]) ",shape=\"box\"" else ""
-      val color = target match {
-        case None => ""
-        case Some(t) =>
-          if (t.transitivePredecessors.contains(e)) {
-            ",color=\"red\""
-          } else {
-            if (t.transitiveSuccessors.contains(e))
-              ",color=\"green\""
-            else {
-              if (e == t)
-                ",color=\"yellow\""
-              else
-                ""
+  // A method that allow to compute a dot file for this test structure
+  def myToDot(target: Option[TestPropagationElement] = None): String = {
+    val labels = elements
+      .map(e => {
+        val shapeString = if (e.isInstanceOf[TestInvariantElement]) ",shape=\"box\"" else ""
+        val color = target match {
+          case None => ""
+          case Some(t) =>
+            if (t.transitivePredecessors.contains(e)) {
+              ",color=\"red\""
+            } else {
+              if (t.transitiveSuccessors.contains(e))
+                ",color=\"green\""
+              else {
+                if (e == t)
+                  ",color=\"yellow\""
+                else
+                  ""
+              }
             }
-          }
-      }
-      s"${e.id} [label=\"${e.name}\"$shapeString$color];"
-    }).mkString("\n    ")
+        }
+        s"${e.id} [label=\"${e.name}\"$shapeString$color];"
+      })
+      .mkString("\n    ")
     val path = pathList
     s"""digraph PropagationStructure {
     $labels
     ${path.map(p => p.mkString("->")).mkString(";\n    ")}
 }
 """
-  }
-
-  override def toDot(
-    names: Map[Int, String] = names,
-    shapes: Map[Int, String] = invariants.map(e => (e.id, "box")).toMap
-  ): String = {
-    super.toDot(names, shapes)
   }
 
   override def registerForPartialPropagation(p: PropagationElement): Unit = {
@@ -70,7 +88,7 @@ class TestPropagationStructure(val debugLevel : Int = 0) extends PropagationStru
     !currentTarget.isDefined
   }
 
-  private def setUpdateRequired : Unit = {
+  private def setUpdateRequired: Unit = {
     for (e <- elements) {
       currentTarget match {
         case None => e.updateRequiredThisPropagation = e.updateRequired
@@ -83,7 +101,7 @@ class TestPropagationStructure(val debugLevel : Int = 0) extends PropagationStru
     }
   }
 
-  private def mkPropagation(reset : Boolean) : Unit = {
+  private def mkPropagation(reset: Boolean): Unit = {
     setUpdateRequired
     propagate(currentTarget.getOrElse(null))
     checkPropagationCount
@@ -91,27 +109,40 @@ class TestPropagationStructure(val debugLevel : Int = 0) extends PropagationStru
       resetPropagationFlags
   }
 
-  def totalPropagation(reset : Boolean = true): Unit = {
+  /** Makes a total propagation
+    *
+    * @param reset
+    *   If reset is true, the propagation flags are reseted after the propagation (they cannot be
+    *   accessed later, e.g. for tests)
+    */
+  def totalPropagation(reset: Boolean = true): Unit = {
     currentTarget = None
     mkPropagation(reset)
   }
 
-  def partialPropagation(target : TestPropagationElement,reset : Boolean = true): Unit = {
-    currentTarget = if (partialPropagationTarget.contains(target) && debugLevel < 3) Some(target) else None
+  /** Makes a partial propagation up to a given target
+    *
+    * @param target
+    *   The target
+    * @param reset
+    *   If reset is true, the propagation flags are reseted after the propagation (they cannot be
+    *   accessed later, e.g. for tests)
+    */
+  def partialPropagation(target: TestPropagationElement, reset: Boolean = true): Unit = {
+    currentTarget =
+      if (partialPropagationTarget.contains(target) && debugLevel < 3) Some(target) else None
     mkPropagation(reset)
   }
 
   private def checkPropagationCount: Unit = elements.foreach(_.checkUpdate)
 
-  def resetPropagationFlags : Unit =
+  /** resets the propagation flags */
+  def resetPropagationFlags: Unit =
     elements.foreach(_.resetFlags)
 
+  /** Closes the propagation structure */
   def close: Unit = {
     setupPropagationStructure()
     closed = true
-  }
-
-  def printUpdates = {
-    println(elements.sortBy(_.id).map(e => s"${e.name}: (Update:${e.updateRequired},UpdateThisTime:${e.updateRequiredThisPropagation},nbUpdate:${e.nbUpdate})").mkString("\n"))
   }
 }
