1d610cf061ccb897501748f8fcaa164d7e7df3e4
TFA <thomas.fayolle@cetic.be>
2024-03-20 14:26:46 +0100

    Adding comments in PropagationInitTestSuite

--- src/test/scala/oscar/cbls/test/core/propagation/PropagationInitTestSuite.scala
+++ src/test/scala/oscar/cbls/test/core/propagation/PropagationInitTestSuite.scala
@@ -2,22 +2,29 @@ package oscar.cbls.test.core.propagation
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import oscar.cbls.core.propagation.{TestVariableElement,TestInvariantElement,TestPropagationStructure}
+import oscar.cbls.core.propagation.{
+  TestInvariantElement,
+  TestPropagationStructure,
+  TestVariableElement
+}
+
+/** Tests about the initialisation of the propagation structure
+  */
 
 class PropagationInitTestSuite extends AnyFunSuite with Matchers {
 
-  val seed : Option[Long] = None
+  val seed: Option[Long] = None
 
   val structureGenerator = new PropagationStructureGenerator(seed)
 
   test("An error shall be raised when there is a cycle in the propagation structure") {
     val struct = new TestPropagationStructure
 
-    val var1 : TestVariableElement = new TestVariableElement(struct)
-    val var2 : TestVariableElement = new TestVariableElement(struct)
-    val var3 : TestVariableElement = new TestVariableElement(struct)
-    val inv1 : TestInvariantElement = new TestInvariantElement(struct)
-    val inv2 : TestInvariantElement = new TestInvariantElement(struct)
+    val var1: TestVariableElement  = new TestVariableElement(struct)
+    val var2: TestVariableElement  = new TestVariableElement(struct)
+    val var3: TestVariableElement  = new TestVariableElement(struct)
+    val inv1: TestInvariantElement = new TestInvariantElement(struct)
+    val inv2: TestInvariantElement = new TestInvariantElement(struct)
 
     inv1.registerStaticAndDynamicDependency(var1)
     var2.setDefiningInvariant(inv1)
@@ -32,12 +39,11 @@ class PropagationInitTestSuite extends AnyFunSuite with Matchers {
     an[java.lang.IllegalArgumentException] should be thrownBy new TestPropagationStructure(4)
     an[java.lang.IllegalArgumentException] should be thrownBy new TestPropagationStructure(-1)
 
-
   }
 
   test("The layer computation algorithm is coherent with the layer computed on build") {
     for (_ <- 0 to 10) {
-      val struct = structureGenerator.generateStructure(15,30,5,15,5,15,5,15,10)
+      val struct = structureGenerator.generateStructure(15, 30, 5, 15, 5, 15, 5, 15, 10)
 
       struct.close
       struct.validateLayerAssignation
