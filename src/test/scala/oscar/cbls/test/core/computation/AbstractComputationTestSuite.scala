4701a5c0264f44664e8a35a83822b61c4096d80b
Germeau Fabian <fabian.germeau@cetic.be>
2024-03-25 16:40:14 +0100

    Remove useless test

--- src/test/scala/oscar/cbls/test/core/computation/AbstractComputationTestSuite.scala
+++ src/test/scala/oscar/cbls/test/core/computation/AbstractComputationTestSuite.scala
@@ -5,23 +5,29 @@ import org.scalatest.matchers.must.Matchers.be
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import oscar.cbls.core.computation.Store
 
-class AbstractComputationTestSuite extends AnyFunSuite{
+class AbstractComputationTestSuite extends AnyFunSuite {
 
   val store = new Store()
 
-  test("Constant variable can not have dynamically listening elements"){
+  test("Constant variable can not have dynamically listening elements") {
     val constantVariable = TestConstantVariable(store)
-    val varyingVariable = TestVaryingVariable(store)
+    val varyingVariable  = TestVaryingVariable(store)
     constantVariable.getTestDynamicallyListeningElements should be(null)
-    val exception = intercept[IllegalArgumentException](constantVariable.registerDynamicallyListeningElement(varyingVariable))
-    assert(exception.getMessage.contains("Constant variable does not propagate, no need to keep track of listening element."))
+    val exception = intercept[IllegalArgumentException](
+      constantVariable.registerDynamicallyListeningElement(varyingVariable)
+    )
+    assert(
+      exception.getMessage.contains(
+        "Constant variable does not propagate, no need to keep track of listening element."
+      )
+    )
   }
 
-  test("Varying variable keep track and can remove dynamically listening elements"){
+  test("Varying variable keep track and can remove dynamically listening elements") {
     val varyingVariable = TestVaryingVariable(store)
-    val variable1 = TestConstantVariable(store)
-    val variable2 = TestVaryingVariable(store)
-    val variable3 = TestInvariant(store)
+    val variable1       = TestConstantVariable(store)
+    val variable2       = TestVaryingVariable(store)
+    val variable3       = TestInvariant(store)
     varyingVariable.getTestDynamicallyListeningElements.isEmpty should be(true)
     val keyForRemoval1 = varyingVariable.registerDynamicallyListeningElement(variable1)
     varyingVariable.registerDynamicallyListeningElement(variable2)
@@ -34,27 +40,16 @@ class AbstractComputationTestSuite extends AnyFunSuite{
     varyingVariable.getTestDynamicallyListeningElements.toList.contains(variable1) should be(false)
   }
 
-  test("Domain definition works as expected"){
+  test("Domain definition works as expected") {
     val variable = TestVaryingVariable(store)
     variable.domain should be(None)
     variable.checkValueWithinDomain(Long.MinValue) should be(true)
     variable.checkValueWithinDomain(Long.MaxValue) should be(true)
-    variable.setDomain(47,251)
+    variable.setDomain(47, 251)
     variable.checkValueWithinDomain(0) should be(false)
     variable.checkValueWithinDomain(487) should be(false)
     variable.checkValueWithinDomain(47) should be(true)
     variable.checkValueWithinDomain(251) should be(true)
     variable.checkValueWithinDomain(100) should be(true)
   }
-
-  test("The Store distinguish the decision variable from the others"){
-    val defined = TestVaryingVariable(store)
-    val listened = TestVaryingVariable(store)
-    val invariant = TestInvariant(store)
-    invariant.addDefinedVariable(defined)
-    invariant.addListenedVariable(listened)
-    store.setupPropagationStructure()
-    store.decisionVariable(listened.)
-  }
-
 }
--- src/test/scala/oscar/cbls/test/core/computation/TestStructures.scala
+++ src/test/scala/oscar/cbls/test/core/computation/TestStructures.scala
@@ -56,7 +56,6 @@ case class TestVaryingVariable(store: Store) extends Variable(store, false) {
 
 case class TestInvariant(store: Store) extends Invariant(store) {
 
-
   def addDefinedVariable(variable: TestVaryingVariable): Unit = {
     variable.setDefiningInvariant(this)
   }
@@ -65,18 +64,9 @@ case class TestInvariant(store: Store) extends Invariant(store) {
     this.registerStaticallyListenedElement(variable)
   }
 
-  /** this is the propagation method that should be overridden by propagation elements. notice that
-   * it is only called in a propagation wave if: 1L: it has been registered for propagation since
-   * the last time it was propagated 2L: it is included in the propagation wave: partial
-   * propagation wave do not propagate all propagation elements; it only propagates the ones that
-   * come in the predecessors of the targeted propagation element overriding this method is
-   * optional, so an empty body is provided by default
-   */
-  override def performPropagation(): Unit = ???
-
   /** This is the debug procedure through which propagation element can redundantly check that the
-   * incremental computation they perform through the performPropagation method is correct
-   * overriding this method is optional, so an empty body is provided by default
-   */
+    * incremental computation they perform through the performPropagation method is correct
+    * overriding this method is optional, so an empty body is provided by default
+    */
   override def checkInternals(): Unit = ???
 }
