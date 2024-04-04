a24d6542d6d8fe2b58e575e27bb8c9fb4bc8afcd
Germeau Fabian <fabian.germeau@cetic.be>
2024-04-04 16:07:38 +0200

    Add the license terms at the top of each class files

--- src/main/scala/oscar/cbls/core/computation/integer/IntIdentityInvariant.scala
+++ src/main/scala/oscar/cbls/core/computation/integer/IntIdentityInvariant.scala
@@ -1,3 +1,16 @@
+// OscaR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// OscaR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License  for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with OscaR.
+// If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+
 package oscar.cbls.core.computation.integer
 
 import oscar.cbls.core.computation.{Invariant, Store}
@@ -26,9 +39,9 @@ class IntIdentityInvariant(model: Store, fromValue: IntVariable, toValue: IntVar
   }
 
   /** This is the debug procedure through which propagation element can redundantly check that the
-   * incremental computation they perform through the performPropagation method is correct
-   * overriding this method is optional, so an empty body is provided by default
-   */
+    * incremental computation they perform through the performPropagation method is correct
+    * overriding this method is optional, so an empty body is provided by default
+    */
   override def checkInternals(): Unit = {
     require(toValue.value() == fromValue.value())
   }
