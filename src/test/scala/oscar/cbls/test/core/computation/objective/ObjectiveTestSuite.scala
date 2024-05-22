b20a10953dd36f4c1c33264f2c318ea8e8578431
Germeau Fabian <fabian.germeau@cetic.be>
2024-05-22 16:09:38 +0200

    Rewrite the DynAndThen test to match  more closely the behavior of the DynAndThen + check if the initial obj value is the proper one when evaluation composition

--- src/test/scala/oscar/cbls/test/core/computation/objective/ObjectiveTestSuite.scala
+++ src/test/scala/oscar/cbls/test/core/computation/objective/ObjectiveTestSuite.scala
@@ -10,72 +10,114 @@ import oscar.cbls.core.search.{Move, MoveFound, NoMoveFound, SearchResult}
 
 class ObjectiveTestSuite extends AnyFunSuite {
 
-  test("Simulating DynAndThen behavior works as expected"){
+  test("Simulating DynAndThen behavior works as expected.") {
 
     // Simulating a DynAndThen process
     // Only the combination of two moves is checked given acceptance criterion
-    val store = new Store()
-    val solValue = IntVariable(store,1000)
+    val store    = new Store()
+    val solValue = IntVariable(store, 1000)
     val minimize = Minimize(solValue)
     store.close()
 
-    var solVariations: List[Long] = List(-800,500,-800,500,-800,500)
-    var summedVariations: Long = 0L
+    var solVariations: List[Long] = Nil
+    var summedVariations: Long    = 1000L
 
-
-    def getMove(exploration: Exploration, solution: IntVariable): SearchResult = {
-      solution :+= solVariations.head
-      summedVariations += solVariations.head
-      solVariations = solVariations.tail
-      exploration.checkNeighbor(objAfter => new DummyMove(objAfter))
-      exploration.toReturn
-    }
-
-    def getDynAndThenMove(exploration: Exploration, obj: IntVariable): SearchResult ={
-      getMove(AcceptAll(obj).newExploration, obj) match {
-        case mf: MoveFound =>
-          mf.objAfter() should be(1000 + summedVariations)
-          getMove(exploration,obj) match {
-            case mf: MoveFound =>
-              mf.objAfter() should be(1000 + summedVariations)
-              mf
-            case NoMoveFound =>
-              require(false, "A move should have been found")
-              NoMoveFound
-          }
-        case NoMoveFound =>
-          require(false, "A move should have been found")
-          NoMoveFound
+    // Test Neighborhood that simply add the provided value to the solution value.
+    class Neighborhood {
+      def getMove(obj: Objective): SearchResult = {
+        val expl = obj.newExploration
+        solValue :+= solVariations.head
+        summedVariations += solVariations.head
+        solVariations = solVariations.tail
+        val move = new DummyMove(solValue.pendingValue)
+        expl.checkNeighbor(_ => move)
+        expl.toReturn
       }
     }
 
-    def startExploration(objective: Objective, skipDynAndThen: Boolean = false): SearchResult ={
-      val expl = objective.newExploration
-      if(skipDynAndThen) getMove(expl, solValue)
-      else getDynAndThenMove(expl, solValue)
-    }
-
-    // Two dynAndThen moves (going from 1000 to 700 (-800 + 500) and from 700 to 400 (-800 + 500)
-    for(_ <- 0 until 2) {
-      val move = startExploration(minimize)
-      move match {
-        case mf: MoveFound =>
-          mf.objAfter() should be(1000 + summedVariations)
-        case NoMoveFound =>
-          require(false, "A resulting move should have been found")
+    // Simulation of a DynAndThen combinator. Basically creating a AcceptFirstEvalComposition and getting a move from left.
+    class DynAndThenTest(left: Neighborhood, right: Move => Neighborhood) extends Neighborhood {
+      override def getMove(baseObj: Objective): SearchResult = {
+        val acceptFirstEvalSecond = new AcceptFirstEvalComposition(baseObj, right)
+        left.getMove(acceptFirstEvalSecond)
       }
     }
 
-    // Now if we commit between the two moves it should fail
-    startExploration(AcceptAll(solValue),skipDynAndThen = true).asInstanceOf[MoveFound].commit()
-    // obj value is now -400
-    // -400 +500 > -400 ==> FAIL
-    startExploration(minimize,skipDynAndThen = true) should be(NoMoveFound)
+    // An empty composite move just to have a "complete" representation of DynAndThen
+    class CompositeMove(firstMove: Move, secondMove: Move, newObjValue: Long) extends Move {
+      override def commit(): Unit   = ???
+      override def objAfter(): Long = ???
+    }
+
+    // Custom objective, accepting first evaluating composition
+    class AcceptFirstEvalComposition(baseObj: Objective, right: Move => Neighborhood) extends Objective {
+      // Creates the base objective exploration, used for composition evaluation.
+      // (Get the initial obj upon Exploration creation)
+      private val baseExploration = baseObj.newExploration
+
+      /** Creates a new Exploration instance. Must be called when starting an exploration. */
+      override def newExploration: Exploration = new Exploration {
+
+        /** Checks if the candidate solution match the acceptance conditions */
+        override def checkNeighbor(buildMove: Long => Move): Unit = {
+          val leftExploredMove = buildMove(0)  // Accept and build move return by right neighborhood
+          // Returned move is right neighborhood's move applied to left neighborhood's move
+          _toReturn = right(leftExploredMove).getMove(new Objective {
+
+            override def newExploration: Exploration = new Exploration {
+
+              /** Checks if the candidate solution match the acceptance conditions*/
+              override def checkNeighbor(buildMove: Long => Move): Unit = {
+                // checkNeighbor uses baseObjective neighbor to evaluate the composition
+                baseExploration.checkNeighbor(buildMove =
+                  newObjValue => new CompositeMove(leftExploredMove, buildMove(newObjValue), newObjValue)
+                )
+                // Needed for Neighborhood's getMove method to return the searchResult
+                _toReturn = baseExploration.toReturn
+              }
+            }
+          })
+
+        }
+      }
+    }
+    val dd = new DynAndThenTest(new Neighborhood, _ => new Neighborhood)
+
+    val dd_3 = new DynAndThenTest(
+      new DynAndThenTest(new Neighborhood, _ => new Neighborhood),
+      _ => new DynAndThenTest(new Neighborhood, _ => new Neighborhood)
+    )
+
+    // Manual testing ==> generating scenario
+    // One DD going from 1000 to 999 (+100,-101) ==> Should be true despite the fact that the first move lead to higher value
+    solVariations = List(100, -101)
+    var searchResult = dd.getMove(minimize)
+    searchResult.isInstanceOf[MoveFound] should be(true)
+    searchResult.asInstanceOf[MoveFound].move.isInstanceOf[CompositeMove] should be(true)
+    solValue.pendingValue should be(999)
+    // One DD going from 999 to 998 (-100,+99) ==> Should be true.
+    // Checks that the original obj value doesn't change it's value with the -100
+    // If it does, the last comparison would be is 899 greater than 998 ? ==> false
+    solVariations = List(-100, 99)
+    searchResult = dd.getMove(minimize)
+    searchResult.isInstanceOf[MoveFound] should be(true)
+    searchResult.asInstanceOf[MoveFound].move.isInstanceOf[CompositeMove] should be(true)
+    solValue.pendingValue should be(998)
+
+    // Three nested DD going from 998 to 997 ((+100 andThen -101) andThen (-101 andThen +101))
+    solVariations = List(100, -101, -101, 101)
+    val searchResult3 = dd_3.getMove(minimize)
+    searchResult3.isInstanceOf[MoveFound] should be(true)
+    searchResult3.asInstanceOf[MoveFound].move.isInstanceOf[CompositeMove] should be(true)
+
+    solValue.pendingValue should be(997)
+    summedVariations should be(solValue.pendingValue)
+
   }
 
 }
 
-private class DummyMove(_objAfter: Long) extends Move{
+private class DummyMove(_objAfter: Long) extends Move {
 
   override def commit(): Unit = {}
 
