172ab8f96160f2975361ae71ee1ac9f9cfe2992f
qme <quentin.meurisse@cetic.be>
2024-05-29 11:15:06 +0200

    Max2 invariant

---
+++ src/main/scala/oscar/cbls/lib/invariant/minmax/Max2.scala
@@ -0,0 +1,69 @@
+// OscaR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// OscaR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License  for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with OscaR.
+// If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+
+package oscar.cbls.lib.invariant.minmax
+
+import oscar.cbls.core.computation.Store
+import oscar.cbls.core.computation.integer.IntVariable
+import oscar.cbls.lib.invariant.numeric.IntInt2Int
+
+/** Companion object of [[Max2]] class. */
+object Max2{
+
+  /** Create a [[Max2]] invariant.
+   *
+   * @param model
+   *  The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+   * @param a
+   *  The first parameter of the function.
+   * @param b
+   *  The second parameter of the function.
+   * @param output
+   *  The [[IntVariable]] which contains max(a, b).
+   * @param name
+   *   The name (optional) of your Invariant.
+   */
+  def apply(model: Store,
+            a: IntVariable,
+            b: IntVariable,
+            output: IntVariable,
+            name: Option[String] = None): Max2 ={
+    new Max2(model, a, b, output, name)
+  }
+}
+
+/** [[oscar.cbls.core.computation.Invariant]] that maintains the maximum of two [[IntVariable]].
+ *
+ * @param model
+ *  The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+ * @param a
+ *  The first parameter of the function.
+ * @param b
+ *  The second parameter of the function.
+ * @param output
+ *  The [[IntVariable]] which contains max(a, b).
+ * @param name
+ *   The name (optional) of your Invariant.
+ */
+class Max2(model: Store,
+           a: IntVariable,
+           b: IntVariable,
+           output: IntVariable,
+           name: Option[String] = None)
+extends IntInt2Int(model, a, b, output, (x: Long, y: Long) => x.max(y), name) {
+
+  override def checkInternals(): Unit = {
+    require(output.value() == a.value().max(b.value()),
+      s"output != max(a, b). output: $output - a: $a - b: $b")
+  }
+}
