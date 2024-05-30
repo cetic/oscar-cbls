667f66df6f3b0dfb8ae1c57e9d7e8d56f05b2bf8
Fabian Germeau <fabian.germeau@cetic.be>
2024-05-30 07:51:21 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- src/main/scala/oscar/cbls/lib/invariant/numeric/Sqrt.scala
+++ src/main/scala/oscar/cbls/lib/invariant/numeric/Sqrt.scala
@@ -28,7 +28,7 @@ object Sqrt {
    * @param input
    *  The listened [[IntVariable]].
    * @param output
-   *  The [[IntVariable]] which contains √(fromValue).
+   *  The [[IntVariable]] which contains √(input).
    * @param name
    *   The name (optional) of your Invariant.
    */
