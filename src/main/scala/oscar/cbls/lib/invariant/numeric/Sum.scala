b83ecaf2f1cad40de368003927eacd5bc2b64aa0
qme <quentin.meurisse@cetic.be>
2024-06-11 10:37:51 +0200

    Sum invariant

--- src/main/scala/oscar/cbls/lib/invariant/numeric/Sum.scala
+++ src/main/scala/oscar/cbls/lib/invariant/numeric/Sum.scala
@@ -17,23 +17,63 @@ import oscar.cbls.core.computation.{IncredibleBulk, Invariant, KeyForRemoval, St
 import oscar.cbls.core.computation.integer.{IntNotificationTarget, IntVariable}
 import oscar.cbls.core.computation.set.{SetNotificationTarget, SetVariable}
 
+/** Companion object of the [[Sum]] class */
+object Sum {
+
+  /** Creates a [[Sum]] invariant
+    *
+    * @param model
+    *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+    * @param input
+    *   An [[Array]] of [[IntVariable]].
+    * @param listenedVariablesIndices
+    *   A [[SetVariable]] containing the indices of the input variables to be listened to calculate
+    *   the sum.
+    * @param output
+    *   The output [[IntVariable]] containing Sum(input(i) | i in listenedVariablesIndices}.
+    * @param bulkIdentifier
+    *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
+    *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one
+    *   if you're referencing the same variables.
+    * @param name
+    *   The name (optional) of your Invariant.
+    */
+  def apply(
+    model: Store,
+    input: Array[IntVariable],
+    listenedVariablesIndices: SetVariable,
+    output: IntVariable,
+    bulkIdentifier: Option[String] = None,
+    name: Option[String] = None
+  ): Sum = {
+    new Sum(model, input, listenedVariablesIndices, output, bulkIdentifier, name)
+  }
+}
 
 /** [[Invariant]] that maintains Sum(input(i) | i in listenedVariablesIndices}
- *
- * @param model
- * @param input
- * @param listenedVariablesIndices
- * @param output
- * @param bulkIdentifier
- * @param name
- *   The name (optional) of your Invariant
- */
+  *
+  * @param model
+  *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+  * @param input
+  *   An [[Array]] of [[IntVariable]].
+  * @param listenedVariablesIndices
+  *   A [[SetVariable]] containing the indices of the input variables to be listened to calculate
+  *   the sum.
+  * @param output
+  *   The output [[IntVariable]] containing Sum(input(i) | i in listenedVariablesIndices}.
+  * @param bulkIdentifier
+  *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
+  *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one if
+  *   you're referencing the same variables.
+  * @param name
+  *   The name (optional) of your Invariant.
+  */
 class Sum(
   model: Store,
   input: Array[IntVariable],
   listenedVariablesIndices: SetVariable,
   output: IntVariable,
-  bulkIdentifier: Option[String],
+  bulkIdentifier: Option[String] = None,
   name: Option[String] = None
 ) extends Invariant(model, name)
     with IntNotificationTarget
@@ -81,8 +121,9 @@ class Sum(
   }
 
   override def checkInternals(): Unit = {
-    val listenedVariables: Set[Long] = listenedVariablesIndices.value().map(i => input(i).value())
-    val expectedSum = listenedVariables.foldLeft(0L)((acc, i) => acc + listenedVariables(i))
+    val listenedVariables: Set[IntVariable] =
+      listenedVariablesIndices.value().map(i => input(i))
+    val expectedSum = listenedVariables.foldLeft(0L)((acc: Long, x: IntVariable) => acc + x.value())
 
     require(
       output.pendingValue == expectedSum,
