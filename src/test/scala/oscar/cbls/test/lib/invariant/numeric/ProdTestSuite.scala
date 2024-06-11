8f8814b938b4cfad87c2c66982181dcc0c81a7d4
qme <quentin.meurisse@cetic.be>
2024-06-11 15:43:17 +0200

    ProdConst unit test

--- src/test/scala/oscar/cbls/test/lib/invariant/numeric/ProdTestSuite.scala
+++ src/test/scala/oscar/cbls/test/lib/invariant/numeric/ProdTestSuite.scala
@@ -25,8 +25,8 @@ class ProdTestSuite extends AnyFunSuite with Matchers {
   private def testProd(
     set: Set[Int]
   ): (Store, Array[IntVariable], SetVariable, IntVariable, Prod) = {
-    val store: Store                          = new Store(debugLevel = 3)
-    val input: Array[IntVariable]             = Array.range(0, 7).map(i => IntVariable(store, i%6))
+    val store: Store              = new Store(debugLevel = 3)
+    val input: Array[IntVariable] = Array.range(0, 7).map(i => IntVariable(store, i % 6))
     val listenedVariablesIndices: SetVariable = SetVariable(store, set)
     val output                                = IntVariable(store, 0)
     val inv                                   = Prod(store, input, listenedVariablesIndices, output)
@@ -35,89 +35,89 @@ class ProdTestSuite extends AnyFunSuite with Matchers {
     (store, input, listenedVariablesIndices, output, inv)
   }
 
-  test("Prod: initialization without null"){
+  test("Prod: initialization without null") {
     val (_, _, _, output, _) = testProd(Set.range(1, 6))
 
     output.value() should be(120)
   }
 
-  test("Prod: initialization with null"){
+  test("Prod: initialization with null") {
     val (_, _, _, output, _) = testProd(Set.range(0, 6))
 
     output.value() should be(0)
   }
 
-  test("Prod: changing non null listened value to another non null value"){
+  test("Prod: changing non null listened value to another non null value") {
     val (_, input, _, output, _) = testProd(Set(2, 3, 4))
     input(4) :+= 3
 
     output.value() should be(42)
   }
 
-  test("Prod: changing a non null value to null"){
+  test("Prod: changing a non null value to null") {
     val (_, input, _, output, _) = testProd(Set(2, 3, 4))
     input(4) := 0
 
     output.value() should be(0)
   }
 
-  test("Prod changing a null to a non null value"){
+  test("Prod changing a null to a non null value") {
     val (_, input, _, output, _) = testProd(Set(0, 2, 3, 4))
     input(0) := 10
 
     output.value() should be(240)
   }
 
-  test("Prod: adding a non null value to a non null product"){
-    val (_, _, lvi, output, _) = testProd(Set(2 , 3))
+  test("Prod: adding a non null value to a non null product") {
+    val (_, _, lvi, output, _) = testProd(Set(2, 3))
     lvi :+= 5
 
     output.value() should be(30)
   }
 
-  test("Prod: adding a value to a null product"){
-    val (_, _, lvi, output, _) = testProd(Set(0, 2 , 3))
+  test("Prod: adding a value to a null product") {
+    val (_, _, lvi, output, _) = testProd(Set(0, 2, 3))
     lvi :+= 5
 
     output.value() should be(0)
   }
 
-  test("Prod: adding a zero to a non null product"){
-    val (_, _, lvi, output, _) = testProd(Set(2 , 3))
+  test("Prod: adding a zero to a non null product") {
+    val (_, _, lvi, output, _) = testProd(Set(2, 3))
     lvi :+= 0
 
     output.value() should be(0)
   }
 
-  test("Prod: removing a non null value from a non null product"){
-    val (_, _, lvi, output, _) = testProd(Set(2 , 3, 4))
+  test("Prod: removing a non null value from a non null product") {
+    val (_, _, lvi, output, _) = testProd(Set(2, 3, 4))
     lvi :-= 3
 
     output.value() should be(8)
   }
 
-  test("Prod: removing a non null value from a null product"){
-    val (_, _, lvi, output, _) = testProd(Set(0, 2 , 3))
+  test("Prod: removing a non null value from a null product") {
+    val (_, _, lvi, output, _) = testProd(Set(0, 2, 3))
     lvi :-= 2
 
     output.value() should be(0)
   }
 
-  test("Prod: removing the only 0 factor from a null product"){
-    val (_, _, lvi, output, _) = testProd(Set(0, 2 , 3))
+  test("Prod: removing the only 0 factor from a null product") {
+    val (_, _, lvi, output, _) = testProd(Set(0, 2, 3))
     lvi :-= 0
 
     output.value() should be(6)
   }
 
-  test("Prod: removing one of the null factors from a null product"){
+  test("Prod: removing one of the null factors from a null product") {
     val (_, _, lvi, output, _) = testProd(Set.range(0, 7))
     lvi :-= 0
 
     output.value() should be(0)
   }
 
-  test("Prod: checkInternal doesn't fail"){
+  test("Prod: checkInternal doesn't fail") {
     val (store, input, lvi, _, inv) = testProd(Set.range(2, 5))
     lvi :+= 5
     lvi :+= 6
@@ -128,5 +128,4 @@ class ProdTestSuite extends AnyFunSuite with Matchers {
 
     inv.checkInternals()
   }
-
 }
