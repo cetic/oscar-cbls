214282852040dc652d3a2eafce52487da49f1c9e
qme <quentin.meurisse@cetic.be>
2024-06-20 09:28:27 +0200

    DenseRef invariant

--- src/main/scala/oscar/cbls/lib/invariant/logic/DenseRef.scala
+++ src/main/scala/oscar/cbls/lib/invariant/logic/DenseRef.scala
@@ -16,31 +16,112 @@ package oscar.cbls.lib.invariant.logic
 import oscar.cbls.core.computation.set.{SetNotificationTarget, SetVariable}
 import oscar.cbls.core.computation.{IncredibleBulk, Invariant, Store}
 
+/** Companion object of [[DenseRef]] class. */
+object DenseRef {
 
-/** [[Invariant]] such that output(i) = {j | i in input(j)}.
- *
- * @param model
- *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
- * @param input
- *   An [[Array]] of [[SetVariable]]
- * @param output
- *   An [[Array]] of [[SetVariable]] such that  output(i) = {j | i in input(j)}.
- * @param bulkIdentifier
- *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
- *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one if
- *   you're referencing the same variables.
- * @param name
- *   The name (optional) of your Invariant
- */
+  /** Creates a [[DenseRef]] invariant.
+    *
+    * @param model
+    *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+    * @param input
+    *   An [[Array]] of [[SetVariable]]
+    * @param output
+    *   An [[Array]] of [[SetVariable]] such that output(i) = {j | i in input(j)}.
+    * @param bulkIdentifier
+    *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
+    *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one
+    *   if you're referencing the same variables.
+    * @param name
+    *   The name (optional) of your Invariant.
+    */
+  def apply(
+    model: Store,
+    input: Array[SetVariable],
+    output: Array[SetVariable],
+    bulkIdentifier: Option[String],
+    name: Option[String]
+  ): DenseRef = {
+    new DenseRef(model, input, output, bulkIdentifier, name)
+  }
+
+  /** Creates a [[DenseRef]] invariant and instantiate the output [[Array]] of [[SetVariable]].
+    *
+    * @param model
+    *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+    * @param input
+    *   An [[Array]] of [[SetVariable]]
+    * @param upperBound
+    *   The [[Int]] such that the input values are in [0, upperBound[.
+    * @param bulkIdentifier
+    *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
+    *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one
+    *   if you're referencing the same variables.
+    * @param name
+    *   The name (optional) of your Invariant.
+    * @return
+    */
+  def makeDenseRef(
+    model: Store,
+    input: Array[SetVariable],
+    upperBound: Int = Int.MaxValue,
+    bulkIdentifier: Option[String],
+    name: Option[String]
+  ): DenseRef = {
+    require(upperBound > 0)
+    val output: Array[SetVariable] = Array.fill(upperBound)(SetVariable(model, Set.empty))
+    new DenseRef(model, input, output, bulkIdentifier, name)
+  }
+}
+
+/** [[Invariant]] such that output(i) = {j | i in input(j)}. It is considered as a dense ref because
+  * output is an array and cover all possible values that can contains the input [[SetVariable]].
+  * Update is in O(1).
+  *
+  * @param model
+  *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
+  * @param input
+  *   An [[Array]] of [[SetVariable]]
+  * @param output
+  *   An [[Array]] of [[SetVariable]] such that output(i) = {j | i in input(j)}.
+  * @param bulkIdentifier
+  *   A [[IncredibleBulk]] is used when several [[Invariant]] listen to vars. Warning:
+  *   [[IncredibleBulk]] are distinguished only by their identifier. Be sure to use the same one if
+  *   you're referencing the same variables.
+  * @param name
+  *   The name (optional) of your Invariant.
+  */
 class DenseRef(
   model: Store,
   input: Array[SetVariable],
-  output: Array[SetVariable],
+  val output: Array[SetVariable], // We need to access it when the invariant is created with
+  // DenseRef.makeDenseRef
   bulkIdentifier: Option[String] = None,
   name: Option[String] = None
 ) extends Invariant(model, name)
     with SetNotificationTarget {
 
+  bulkIdentifier match {
+    case None =>
+      // No bulk is used
+      for (vars <- input) this.registerStaticallyListenedElement(vars)
+    case Some(bulkId) =>
+      // Register static dependency via a bulk
+      this.addIncredibleBulk(IncredibleBulk.bulkRegistering(input, bulkId, model))
+  }
+
+  for (ref <- output) {
+    ref.setDefiningInvariant(this)
+    ref := Set.empty
+  }
+
+  for (j <- input.indices) {
+    input(j).registerDynamicallyListeningElement(this, j)
+    for (ref <- input(j).value()) {
+      output(ref) :+= j
+    }
+  }
+
+  @inline
   override def notifySetChanges(
     setVariable: SetVariable,
     index: Int,
@@ -48,7 +129,23 @@ class DenseRef(
     removedElems: Iterable[Int],
     oldValue: Set[Int],
     newValue: Set[Int]
-  ): Unit = ???
+  ): Unit = {
+    for (added   <- addedElems) output(added) :+= index
+    for (removed <- removedElems) output(removed) :-= index
+  }
 
-  override def checkInternals(): Unit = ???
+  override def checkInternals(): Unit = {
+    // output(i) == {j | i in input(j)}
+    for (i <- output.indices) {
+      for (j <- output(i).pendingValue) {
+        require(
+          input(j).value().contains(i),
+          s"checkInternals fails in invariant ${name()}. " +
+            s"output is not referencing to the good input set. " +
+            s"ref: $i - output(i): ${output(i).pendingValue} - input: ${input.mkString("", ", ", "")}"
+        )
+      }
+    }
+  }
+
 }
