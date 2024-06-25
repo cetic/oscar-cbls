9b638c0d33b8a270753ab6542aef556d64adf311
TFA <thomas.fayolle@cetic.be>
2024-06-25 14:45:36 +0200

    Adding test for all operations

--- src/test/scala/oscar/cbls/test/algo/dll/DLLTestCommands.scala
+++ src/test/scala/oscar/cbls/test/algo/dll/DLLTestCommands.scala
@@ -8,27 +8,73 @@ import oscar.cbls.algo.dll.DoublyLinkedList
 import scala.util.{Failure, Success, Try}
 
 import scala.annotation.tailrec
+import org.scalacheck.Arbitrary.arbitrary
+
+/** Defines the System Under Test (SUT)
+  *
+  * In the DLL case, the SUT contains a DLL and a classical list that acts as witness. Both list are
+  * modified the same way and methods allow to compare the lists to ensure that the two lists are
+  * identical after the movements
+  *
+  * @param dll
+  *   The dll to test
+  * @param l
+  *   The witness list to compare with
+  */
 
 case class DllTestStruct(dll: DoublyLinkedList[Int], var l: List[Int]) {
 
+  var containerList: List[dll.DLLStorageElement] = List()
+
   def insertStart(elem: Int) = {
-    dll.insertStart(elem)
+    val container = dll.insertStart(elem)
+    containerList = container :: containerList
     l = elem :: l
   }
 
   def insertEnd(elem: Int) = {
-    dll.insertEnd(elem)
-    l = (elem :: l.reverse).reverse
+    val container = dll.insertEnd(elem)
+    containerList = containerList.appended(container)
+    l = l.appended(elem)
+  }
+
+  def insertAfter(elem: Int, afterPos: Int) = {
+    require(afterPos >= 0,s"InsertAfter($afterPos) impossible in $l")
+    require(afterPos < l.size,s"InsertAfter($afterPos) impossible in $l")
+    val (l1, l2) = l.splitAt(afterPos +1)
+    l = l1 ::: (elem :: l2)
+    val (c1, c2)  = containerList.splitAt(afterPos+1)
+    val container = dll.insertAfter(elem, containerList(afterPos))
+    containerList = c1 ::: (container :: c2)
   }
 
   def removeStart = {
     dll.popStart()
     l = l.tail
+    containerList = containerList.tail
+  }
+
+  def removeEnd = {
+    dll.popEnd()
+    l = l.reverse.tail.reverse
+    containerList = containerList.reverse.tail.reverse
+  }
+
+  def removePos(pos: Int) = {
+    require(pos >= 0,s"RemovePos($pos) impossible in $l")
+    require(pos < l.size,s"RemovePos($pos) impossible in $l")
+    val cont = containerList(pos)
+    cont.delete()
+    val (c1, c2) = containerList.splitAt(pos)
+    val (l1, l2) = l.splitAt(pos)
+    containerList = c1 ::: c2.tail
+    l = l1 ::: l2.tail
   }
 
   def dropAll() = {
     dll.dropAll()
     l = Nil
+    containerList = Nil
   }
 
   /** compares the size of the dll and the size of its witness list */
@@ -58,16 +104,19 @@ case class DllTestStruct(dll: DoublyLinkedList[Int], var l: List[Int]) {
 
 }
 
+/** On object that defines the commands to use on the lists
+  */
+
 object DLLTestCommands extends Commands {
 
   type State = Int
 
+  // The type of the System Under Test (SUT)
   type Sut = DllTestStruct
 
   val rand = new scala.util.Random(1000)
 
-  private val genElement = Gen.choose(1, 100)
-
+  // Defining the generators
   private val genOpEmpty: Gen[Command] = for {
     v <- Gen.choose(1, 100)
     c <- Gen.oneOf(List(AddStart(v), AddEnd(v), DropAllOperation))
@@ -75,18 +124,48 @@ object DLLTestCommands extends Commands {
 
   private val genRemove: Gen[Command] = Gen.oneOf(List(RemoveStart))
 
+  // Defines the precondition to define a new SUT
   override def canCreateNewSut(
     newState: State,
-    initSuts: Traversable[State],
-    runningSuts: Traversable[Sut]
+    initSuts: Iterable[State],
+    runningSuts: Iterable[Sut]
   ): Boolean = true
 
+  // How to destruy a SUT
   override def destroySut(sut: Sut): Unit = ()
 
-  override def genCommand(state: State): Gen[Command] = if (state == 0)
-    genOpEmpty
-  else
-    Gen.oneOf(genOpEmpty, genRemove)
+  // How to generate a command (according to the state)
+  override def genCommand(state: State): Gen[Command] = {
+    val genAddStart: Gen[Command] = for {
+      v <- Gen.choose(0,100)
+    } yield AddStart(v)
+    val genAddEnd: Gen[Command] = for {
+      v <- Gen.choose(0,100)
+    } yield AddEnd(v)
+    val genRemoveAll = Gen.const(DropAllOperation)
+    if (state == 0) {
+      Gen.oneOf(genAddStart, genAddEnd, genRemoveAll)
+    } else {
+      val genAddPos: Gen[Command] = for {
+        v   <- Gen.choose(0,100)
+        pos <- Gen.choose(0, state -1)
+      } yield AddAfter(v, pos)
+      val genRemoveStart = Gen.const(RemoveStart)
+      val genRemoveEnd   = Gen.const(RemoveEnd)
+      val genRemovePos = for {
+        pos <- Gen.choose(0, state - 1)
+      } yield RemovePos(pos)
+      Gen.oneOf(
+        genAddStart,
+        genAddEnd,
+        genRemoveAll,
+        genAddPos,
+        genRemoveStart,
+        genRemoveEnd,
+        genRemovePos
+      )
+    }
+  }
 
   override def genInitialState: Gen[State] =
     Gen.const(0)
@@ -125,13 +204,19 @@ object DLLTestCommands extends Commands {
       sut
     }
 
-    override def toString = s"AddStart($value)"
-
   }
 
   case class AddEnd(value: Int) extends AddOperation {
     override def run(sut: Sut): Result = {
       sut.insertEnd(value)
+      sut
+    }
+
+  }
+
+  case class AddAfter(value: Int, pos: Int) extends AddOperation {
+    override def run(sut: Sut): Result = {
+      sut.insertAfter(value, pos)
       sut
     }
   }
@@ -151,6 +236,13 @@ object DLLTestCommands extends Commands {
     override def nextState(state: State): State = state - 1
   }
 
+  case object RemoveEnd extends RemoveOperation {
+    override def run(sut: Sut): Result = {
+      sut.removeEnd
+      sut
+    }
+  }
+
   case object RemoveStart extends RemoveOperation {
 
     override def run(sut: Sut): Result = {
@@ -158,6 +250,13 @@ object DLLTestCommands extends Commands {
       sut
     }
 
+  }
+
+  case class RemovePos(p: Int) extends RemoveOperation {
+    override def run(sut: Sut): Result = {
+      sut.removePos(p)
+      sut
+    }
   }
 
 }
