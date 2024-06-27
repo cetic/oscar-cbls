9afb95386f5d00ecb0d21076cba476c4f649c45f
qme <quentin.meurisse@cetic.be>
2024-06-27 13:13:48 +0200

    review Prod and ProdTestSuite

--- src/main/scala/oscar/cbls/lib/invariant/numeric/Prod.scala
+++ src/main/scala/oscar/cbls/lib/invariant/numeric/Prod.scala
@@ -20,24 +20,24 @@ import oscar.cbls.core.computation.set.{SetNotificationTarget, SetVariable}
 /** Companion object of the [[Prod]] class. */
 object Prod {
 
-  /** Creates a Prod invariant.
+  /** Creates a `Prod` invariant, which maintains `Prod(input(i) | i in listenedVariablesIndices)`,
+    * * where `input` is an array of IntVariables.
     *
     * @param model
     *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
     * @param input
-    *   The elements we want to multiply.
+    *   The array of variables to multiply.
     * @param listenedVariablesIndices
-    *   A SetVariable containing the indices of the input variables to be listened to calculate the
-    *   product.
+    *   A SetVariable containing the indices of the input variables to multiply.
     * @param output
-    *   The output variable containing Prod(input(i) | i in listenedVariablesIndices).
+    *   The output variable containing `Prod(input(i) | i in listenedVariablesIndices)`.
     * @param bulkIdentifier
     *   A [[oscar.cbls.core.computation.IncredibleBulk]] is used when several
     *   [[oscar.cbls.core.computation.Invariant]] listen to vars. Warning:
     *   [[oscar.cbls.core.computation.IncredibleBulk]] are distinguished only by their identifier.
     *   Be sure to use the same one if you're referencing the same variables.
     * @param name
-    *   The name (optional) of your Invariant.
+    *   The name (optional) of the Invariant.
     */
   def apply(
     model: Store,
@@ -51,25 +51,24 @@ object Prod {
   }
 }
 
-/** [[oscar.cbls.core.computation.Invariant]] that maintains Prod(input(i) | i in
-  * listenedVariablesIndices). Update is in O(1).
+/** [[oscar.cbls.core.computation.Invariant]] that maintains `Prod(input(i) | i in``
+  * `listenedVariablesIndices)`. Update is in O(1).
   *
   * @param model
   *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this invariant is linked.
   * @param input
-  *   The elements we want to multiply.
+  *   The array of variables to multiply.
   * @param listenedVariablesIndices
-  *   A SetVariable containing the indices of the input variables to be listened to calculate the
-  *   product.
+  *   A SetVariable containing the indices of the input variables to multiply.
   * @param output
-  *   The output variable containing Prod(input(i) | i in listenedVariablesIndices).
+  *   The output variable containing `Prod(input(i) | i in listenedVariablesIndices)`.
   * @param bulkIdentifier
   *   A [[oscar.cbls.core.computation.IncredibleBulk]] is used when several
   *   [[oscar.cbls.core.computation.Invariant]] listen to vars. Warning:
   *   [[oscar.cbls.core.computation.IncredibleBulk]] are distinguished only by their identifier. Be
   *   sure to use the same one if you're referencing the same variables.
   * @param name
-  *   The name (optional) of your Invariant.
+  *   The name (optional) of the Invariant.
   */
 class Prod(
   model: Store,
@@ -106,7 +105,6 @@ class Prod(
 
   updateOutput()
 
-  @inline
   override def notifyIntChanges(
     intVariable: IntVariable,
     contextualVarIndex: Int,
@@ -115,6 +113,7 @@ class Prod(
   ): Unit = {
     // The update mechanism of an IntVariable is such that oldVal is never equal to newVal.
     // So the case oldVal == 0 && newVal == 0 never happens.
+    assert(oldVal != newVal)
     if (oldVal == 0 && newVal != 0) {
       numberOfZeroFactors -= 1
       nonZeroProduct *= newVal
@@ -128,7 +127,6 @@ class Prod(
     updateOutput()
   }
 
-  @inline
   override def notifySetChanges(
     setVariable: SetVariable,
     index: Int,
@@ -158,13 +156,14 @@ class Prod(
   }
 
   @inline
-  private[this] def updateOutput(): Unit = {
+  final private[this] def updateOutput(): Unit = {
     if (numberOfZeroFactors == 0) output := nonZeroProduct
     else output                          := 0
   }
 
+  // updates product when an additional IntVariable must be used
   @inline
-  private[this] def notifyInsertOn(set: SetVariable, index: Int): Unit = {
+  final private[this] def notifyInsertOn(set: SetVariable, index: Int): Unit = {
     assert(set == listenedVariablesIndices)
 
     keysForRemoval(index) = input(index).registerDynamicallyListeningElement(this, index)
@@ -175,12 +174,12 @@ class Prod(
     updateOutput()
   }
 
+  // updates product when an IntVariable is not used anymore
   @inline
-  private[this] def notifyDeleteOn(set: SetVariable, index: Int): Unit = {
+  final private[this] def notifyDeleteOn(set: SetVariable, index: Int): Unit = {
     assert(set == listenedVariablesIndices)
 
     keysForRemoval(index).delete()
-    keysForRemoval(index) = null
 
     if (input(index).value() == 0) numberOfZeroFactors -= 1
     else nonZeroProduct /= input(index).value()
