44630d73a5a7c1c0b8eca8e886dc4e3715869f64
qme <quentin.meurisse@cetic.be>
2024-09-12 08:15:11 +0200

    companion object of SetConstant

--- src/main/scala/oscar/cbls/core/computation/set/SetConstant.scala
+++ src/main/scala/oscar/cbls/core/computation/set/SetConstant.scala
@@ -15,6 +15,22 @@ package oscar.cbls.core.computation.set
 
 import oscar.cbls.core.computation.Store
 
+/** Companion object of [[SetConstant]]. */
+object SetConstant {
+
+  /** Creates a SetConstant.
+    *
+    * Since it's a constant, invoking methods that attempt to change its value will throw an *
+    * exception.
+    *
+    * @param model
+    *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this variable is linked.
+    * @param value
+    *   The value of this constant variable.
+    */
+  def apply(model: Store, value: Set[Int]): SetConstant = new SetConstant(model, value)
+}
+
 /** A constant SetVariable.
   *
   * Since it's a constant, invoking methods that attempt to change its value will throw an
@@ -23,7 +39,7 @@ import oscar.cbls.core.computation.Store
   * @param model
   *   The [[oscar.cbls.core.propagation.PropagationStructure]] to which this variable is linked.
   * @param value
-  *   The value of this constant variable
+  *   The value of this constant variable.
   */
 class SetConstant(model: Store, value: Set[Int]) extends SetVariable(model, value, true) {
   override protected def setValue(value: Set[Int]): Unit = {
